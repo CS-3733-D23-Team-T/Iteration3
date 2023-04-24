7cd88ec3355bc7ca61967b51d37c2a6a25fd46d1
vanessasam1 <vasam@wpi.edu>
2023-04-24 16:44:18 -0400

    fixed credit controller but still need to make into hyperlinks

--- src/main/java/edu/wpi/tacticaltritons/controllers/CreditsController.java
+++ src/main/java/edu/wpi/tacticaltritons/controllers/CreditsController.java
@@ -1,4 +1,31 @@
 package edu.wpi.tacticaltritons.controllers;
 
+import edu.wpi.tacticaltritons.App;
+import io.github.palexdev.materialfx.controls.MFXScrollPane;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+
 public class CreditsController {
+
+    @FXML private MFXScrollPane creditText;
+    @FXML
+    public void initialize() {
+        creditText.setFitToHeight(true);
+        creditText.setFitToWidth(true);
+        App.getPrimaryStage().widthProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                creditText.setMinWidth(newValue.doubleValue());
+            }
+        });
+        App.getPrimaryStage().heightProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue) {
+                creditText.setMinHeight(newValue.doubleValue());
+            }
+        });
+    }
+    private void resize(){
+    }
 }
