0083f38258d95d59329d637d68a40c4c1b88aacf
james <jamesstaryi@yahoo.com>
2023-04-24 21:32:27 -0400

    fixed bug

--- src/main/java/edu/wpi/tacticaltritons/database/daoImplementations/MoveDaoImpl.java
+++ src/main/java/edu/wpi/tacticaltritons/database/daoImplementations/MoveDaoImpl.java
@@ -106,13 +106,12 @@ public class MoveDaoImpl implements MoveDao {
       connection = Tdb.getConnection();
 
 
-      String sql = "create or replace view CurrentLocation as " +
-              "select nodeid, longname, max(date) as moveDate " +
-              "from Move " +
-              "where date <= ? " +
-              "group by nodeid, longname; " +
-              "select * " +
-              "from CurrentLocation;";
+      String sql = "SELECT m.nodeID, m.longName, m.date, l.shortName, l.nodeType, n.xcoord, n.ycoord, n.floor, n.building "
+              + "FROM Move m "
+              + "JOIN Node n ON m.nodeID = n.nodeID "
+              + "JOIN LocationName l ON m.longName = l.longName "
+              + "WHERE m.date <= ? "
+              + "ORDER BY m.date;";
       ps = connection.prepareStatement(sql);
 
       ps.setDate(1, currentDate);
