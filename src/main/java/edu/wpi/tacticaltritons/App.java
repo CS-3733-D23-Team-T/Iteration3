fe3deeca89a5e2607ac2d29bed95d47eb8274f74
apollo <1apollo.rowe@gmail.com>
2023-04-27 05:49:18 -0400

    yes

--- src/main/java/edu/wpi/tacticaltritons/App.java
+++ src/main/java/edu/wpi/tacticaltritons/App.java
@@ -1,24 +1,41 @@
 package edu.wpi.tacticaltritons;
 
+import com.google.common.util.concurrent.AtomicDouble;
 import edu.wpi.tacticaltritons.data.FlowerHashMap;
 import edu.wpi.tacticaltritons.data.FurnitureHashMap;
 import edu.wpi.tacticaltritons.data.QuickNavigationMenuButtons;
 import edu.wpi.tacticaltritons.database.Tdb;
 import edu.wpi.tacticaltritons.navigation.Screen;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import javafx.geometry.Point2D;
+import javafx.scene.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.PhongMaterial;
+import javafx.scene.shape.Box;
+import javafx.scene.shape.DrawMode;
+import javafx.scene.shape.Sphere;
+import javafx.scene.transform.Rotate;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
 
 import java.io.IOException;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicReference;
 
 @Slf4j
 public class App extends Application {
@@ -50,7 +67,7 @@ public class App extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) throws IOException {
+  public void start(Stage primaryStage) throws Exception {
     //    /* primaryStage is generally only used if one of your components require the stage to
     App.primaryStage = primaryStage;
     quickNavigationMenuButtons = new QuickNavigationMenuButtons();
@@ -98,7 +115,8 @@ public class App extends Application {
     loader = new FXMLLoader(App.class.getResource("views/navigation/AdminQuickNavigation.fxml"));
     adminQuickNavigation = loader.load();
 
-    final Scene scene = new Scene(rootPane);
+
+    final Scene scene = new Scene(createContent());
     primaryStage.setScene(scene);
     primaryStage.setTitle("CS3733 - Tactical Tritons");
     primaryStage.show();
@@ -109,4 +127,89 @@ public class App extends Application {
     Tdb.getConnection().close();
     log.info("Shutting Down");
   }
+
+  private Parent createContent() throws Exception {
+    Sphere sphere = new Sphere(2.5);
+    sphere.setMaterial(new PhongMaterial(Color.FORESTGREEN));
+
+    sphere.setTranslateZ(7);
+    sphere.setTranslateX(2);
+
+    Box box = new Box(5, 5, 5);
+    box.setMaterial(new PhongMaterial(Color.RED));
+
+    Translate pivot = new Translate();
+    Rotate yRotate = new Rotate(0, Rotate.Y_AXIS);
+    Rotate xRotate = new Rotate(0, Rotate.X_AXIS);
+
+    // Create and position camera
+    PerspectiveCamera camera = new PerspectiveCamera(true);
+    camera.getTransforms().addAll (
+            pivot,
+            yRotate,
+            xRotate,
+            new Translate(0, 0, -50)
+    );
+
+    // Build the Scene Graph
+    Group root = new Group();
+    root.getChildren().add(camera);
+    root.getChildren().add(box);
+    root.getChildren().add(sphere);
+
+    // set the pivot for the camera position animation base upon mouse clicks on objects
+
+
+    // Use a SubScene
+    SubScene subScene = new SubScene(
+            root,
+            300,300,
+            true,
+            SceneAntialiasing.BALANCED
+    );
+    subScene.setFill(Color.ALICEBLUE);
+    subScene.setCamera(camera);
+    Group group = new Group();
+    group.getChildren().add(subScene);
+
+    List<Point2D> points = new ArrayList<>();
+    subScene.setOnMouseDragged(event -> {
+      System.out.println("(" + event.getSceneX() + ", " + event.getSceneY() + ")");
+      if(points.size() == 0){
+        points.add(new Point2D(event.getSceneX(), event.getSceneY()));
+      }
+      else{
+        Point2D secondPoint = new Point2D(event.getSceneX(), event.getSceneY());
+
+        double hyp = points.get(0).distance(secondPoint);
+        double opp = Math.abs(secondPoint.getY() - points.get(0).getY());
+        double adj = Math.abs(secondPoint.getX() - points.get(0).getX());
+
+        double magnitude = hyp / Math.sqrt(subScene.getWidth() * subScene.getHeight());
+
+        double xMultiplier = secondPoint.getY() < points.get(0).getY() ? 1 : -1;
+        double xAngle = magnitude * xMultiplier * Math.atan(opp/adj) * (180 / Math.PI);
+
+        double yMultiplier = secondPoint.getX() < points.get(0).getX() ? -1 : 1;
+        double yAngle = magnitude * yMultiplier * Math.atan(adj/opp) * (180 / Math.PI);
+
+        camera.getTransforms().stream().filter(node -> (node instanceof Rotate)).forEach(node -> {
+          if(((Rotate) node).getAxis().equals(Rotate.X_AXIS) && !Double.isNaN(xAngle)){
+            ((Rotate) node).setAngle(((Rotate) node).getAngle() + xAngle);
+          }
+          else if(((Rotate) node).getAxis().equals(Rotate.Y_AXIS) && !Double.isNaN(yAngle)){
+            ((Rotate) node).setAngle(((Rotate) node).getAngle() + yAngle);
+          }
+        });
+
+
+        System.out.println("X Translate Angle = " + xAngle + ", Y Translate Angle = " + yAngle);
+
+        points.clear();
+      }
+    });
+
+    return group;
+  }
+
 }
