c076c1616a06b97396c6d6581a71d2c83ab6a40d
vanessasam1 <vasam@wpi.edu>
2023-04-28 01:03:31 -0400

    connections code

--- src/main/java/edu/wpi/tacticaltritons/database/Tdb.java
+++ src/main/java/edu/wpi/tacticaltritons/database/Tdb.java
@@ -1,35 +1,34 @@
 package edu.wpi.tacticaltritons.database;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.sql.*;
 
 public enum Tdb {
+  AWS("jdbc:postgresql://softeng.c280prrsyadq.us-east-2.rds.amazonaws.com:5432/postgres", "teamt", "CS3733yay"),
+  WPI_DB("jdbc:postgresql://database.cs.wpi.edu:5432/teamtdb", "teamt", "teamt170");
 
-  INSTANCE;
-  private static final String url = "jdbc:postgresql://database.cs.wpi.edu:5432/teamtdb";
-  private static final String user = "teamt";
-  private static final String pass = "teamt170";
+  private final String url;
+  private final String user;
+  private final String pass;
+  private static Tdb INSTANCE = WPI_DB;
+  Tdb(String url, String user, String pass){
+    this.url = url;
+    this.user = user;
+    this.pass = pass;
+  }
+  
+  public static Tdb getInstance(){
+    return INSTANCE;
+  }
+  public static void setInstance(Tdb instance){
+    INSTANCE = instance;
+  }
 
-
-  private static Connection connection;
-
-
-  public static Connection getConnection() throws SQLException, ClassNotFoundException {
+  public Connection getConnection() throws SQLException, ClassNotFoundException {
     Class.forName("org.postgresql.Driver");
-    if(connection == null || connection.isClosed()){
-      connection = createConnection();
-    }
-    return connection;
+    return DriverManager.getConnection(this.url, this.user, this.pass);
   }
 
-  private static Connection createConnection() {
-    try {
-      Class.forName("org.postgresql.Driver");
-      connection = DriverManager.getConnection(url, user, pass);
-    } catch (SQLException e) {
-      e.printStackTrace();
-    } catch (ClassNotFoundException e) {
-      throw new RuntimeException(e);
-    }
-    return connection;
-  }
 }
